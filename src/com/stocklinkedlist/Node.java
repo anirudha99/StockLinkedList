f0d4e7c27298d199ba9d146fedbb30bd170203e3
anirudha mayya <anirudha.mayya@gmail.com>
2021-09-16 23:14:06 +0530

    Stock with linked list

---
+++ src/com/stocklinkedlist/Node.java
@@ -0,0 +1,17 @@
+package com.stocklinkedlist;
+
+public class Node<T> {
+	
+	T data;
+	Node<T> next;
+
+	Node(T data) {
+		this.data = data;
+		next = null;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+}
---
+++ src/com/stocklinkedlist/StockInterface.java
@@ -0,0 +1,15 @@
+package com.stocklinkedlist;
+
+public interface StockInterface {
+	
+	double valueOf();
+
+	void buy(int amount, String symbol);
+
+	void sell(int amount, String symbol);
+
+	void save(String filename);
+
+	void printReport();
+
+}
